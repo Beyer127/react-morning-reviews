2e34cd2b87076501af96a016e71b7c8abeea7c9e
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2020-07-22 09:33:26 -0700

    React-Morning-Reviews

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import "./App.css"
+import List from "./components/List"
 
+const App = () => {
+    return(
+        <div className="App">
+            <h1 className="movie-list-title">The most amazing movie list</h1>
+            <List/>
+        </div>
+    )
+}
+
+export default App
